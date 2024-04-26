75757bd175b66328ef9788c72495a69483e43eda
Khasan Kholmatov <kholmatovkh99@gmail.com>
2024-04-27 04:36:41 +0500

    logo fix

--- components/layout/LeftSideBar.tsx
+++ components/layout/LeftSideBar.tsx
@@ -12,7 +12,7 @@ const LeftSideBar = () => {
 
   return (
     <div className="h-screen left-0 top-0 sticky p-10 flex flex-col gap-16 bg-blue-2 shadow-xl max-lg:hidden">
-      <Image src="/logo.png" alt="logo" width={150} height={70} />
+      <Image src="/logo.png" alt="logo" width={150} height={70} priority={false}/>
 
       <div className="flex flex-col gap-12">
         {navLinks.map((link) => (
